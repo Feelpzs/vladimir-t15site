df5d307ce347454bafa02f16c67cee3e9e09e30c
Feelpzs <felipeworkejobs@gmail.com>
2025-07-25 11:00:55 -0300

    Implementacao do j.son do champs.

--- lobby.js
+++ lobby.js
@@ -4,47 +4,14 @@ const painel = document.getElementById("painel-matchup");
 const nomeCampeao = document.getElementById("nome-campeao");
 
 // Objeto que armazena os dados de cada campeão (você preenche para cada um)
-const matchups = {
-    aatrox: {
-        matchupTexto: [
-            "O Q do Aatrox é previsível, desvie com a Poça.",
-            "Evite trocas longas no early game, ele é mais forte.",
-            "Abuse do sustain com seu Q para trocar dano aos poucos."
-        ],
-        spells: [
-            "imglobby/spells/ghost.webp",
-            "imglobby/spells/flash.png"
-        ],
-        build: [
-            "imglobby/itens/cinturao hextec.webp",
-            "imglobby/itens/foco do horizonte.webp",
-            "imglobby/itens/rabadon.webp",
-            "imglobby/itens/cajado do vazio.webp",
-            "imglobby/itens/chama sombria.webp",
-        ],
-        runasPrimaria: [
-            "imglobby/runas/aery.webp",
-            "imglobby/runas/manto.webp",
-            "imglobby/runas/chamuscar.png",
-            "imglobby/runas/transcedencia.webp",
-            
+fetch("matchups_completo.json")
+  .then(response => response.json())
+  .then(data => {
+      matchups = data;
+      console.log("Matchups carregados com sucesso!", matchups);
+  })
+  .catch(err => console.error("Erro ao carregar o JSON:", err));
 
-        ],
-        runasSecundaria: [
-            "imglobby/runas/lenda.webp",
-            "imglobby/runas/ate a morte.webp"
-        ],
-        runasStats: [
-            "imglobby/runas/cdr.png",
-            "imglobby/runas/forca adaptativa.png",
-            "imglobby/runas/vida escalavel.png"
-            
-        ],
-        cooldowns: "Q – 14/12/10 | W – 20/18/16 | E – 9/8/7 | R – 120/100/80",
-        video: "https://www.youtube.com/watch?v=v853NtnESYo"
-    }
-    // Adicione os outros campeões aqui...
-};
 
 
 // Evento de clique nos campeões
@@ -127,4 +94,15 @@ champs.forEach(champ => {
         painel.scrollIntoView({ behavior: "smooth" });
     });
 });
+// localizar champ
+const searchInput = document.querySelector("#search-champ");
+const champ = document.querySelectorAll(".champ");
 
+searchInput.addEventListener("input", () => {
+    const searchTerm = searchInput.value.toLowerCase();
+    
+    champs.forEach(champ => {
+        const champName = champ.dataset.campeao.toLowerCase();
+        champ.style.display = champName.includes(searchTerm) ? "inline-block" : "none";
+    });
+});
